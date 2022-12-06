11dc913a8b176fa22c02ae9e2bd2dcc84cd0a840
ismael-soler <ismaelsolerferreira@gmail.com>
2022-12-06 18:53:52 -0300

    commit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,3 +7,24 @@ from flask import jsonify
 @app_views.route("/status")
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    from models.__init__ import storage
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    return jsonify(
+        {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count("Amenity"),
+            "reviews": storage.count("Amenity"),
+            "states": storage.count("Amenity"),
+            "users": storage.count("Amenity")
+        }
+    )
